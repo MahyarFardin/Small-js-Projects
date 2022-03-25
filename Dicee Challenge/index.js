09bc503043936891580db5756710d8447a95176d
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-25 14:38:14 +0430

    added the screen shot

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,8 +1,11 @@
 var dice1 = Math.ceil(Math.random() * 6);
 var dice2 = Math.ceil(Math.random() * 6);
-
-const img1=document.querySelectorAll(".dice img")[0].setAttribute("src", "/images/dice"+dice1+".png");
-const img2=document.querySelectorAll(".dice img")[1].setAttribute("src", "/images/dice"+dice2+".png");
+var src1="/images/dice"+dice1+".png";
+var src2="/images/dice"+dice1+".png";
+const img1=document.querySelectorAll(".dice img")[0];
+img1.setAttribute("src", src1);
+const img2=document.querySelectorAll(".dice img")[1];
+img2.setAttribute("src", src2);
 
 var winner =document.querySelector(".container h3"); 
 
