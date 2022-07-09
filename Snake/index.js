aed30d5c48795daa64fea11cfe74b3d8391b13c5
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-09 22:31:35 +0430

    done with moving and scoring

--- Snake/index.js
+++ Snake/index.js
@@ -6,17 +6,19 @@ const snakeColor = "#08D9D6"
 const snakeBorder = "#252A34"
 const appleColor = "#EAEAEA"
 const unitSize = 25
+const scoreOnDoc = document.getElementById("score")
+
 let running = false
 let score = 0
 let xSpeed = unitSize
 let ySpeed = 0
 let food = { x: 0, y: 0 }
 let snake = [
-    { x: 0, y: 0 },
-    { x: unitSize, y: 0 },
-    { x: unitSize * 2, y: 0 },
+    { x: unitSize * 4, y: 0 },
     { x: unitSize * 3, y: 0 },
-    { x: unitSize * 4, y: 0 }
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 }
 ]
 
 startGame();
@@ -35,33 +37,38 @@ function snakeDrawer() {
 function appleDrop() {
     food.x = Math.floor(Math.random() * 500 / unitSize) * unitSize
     food.y = Math.floor(Math.random() * 500 / unitSize) * unitSize
-
+}
+function appleDrow() {
     ctx.fillStyle = appleColor
     ctx.fillRect(food.x, food.y, unitSize, unitSize)
-
 }
 function directionShifter(e) {
     switch (e.keyCode) {
+        case 37:
+            wrong.style.display = "none"
+            xSpeed = -unitSize
+            ySpeed = 0
+            break
         case 38:
             wrong.style.display = "none"
-
+            xSpeed = 0
+            ySpeed = -unitSize
             break
         case 39:
             wrong.style.display = "none"
-
+            xSpeed = unitSize
+            ySpeed = 0
             break
         case 40:
             wrong.style.display = "none"
-
-            break
-        case 37:
-            wrong.style.display = "none"
-
+            xSpeed = 0
+            ySpeed = unitSize
             break
         default:
             wrong.style.display = "inline-block"
     }
 }
+
 function snakeMover() {
     const head = {
         x: snake[0].x + xSpeed,
@@ -70,9 +77,11 @@ function snakeMover() {
 
     snake.unshift(head)
 
-    if (snake[0].x === food.x && snake[0].y === food.y) {
+    if (snake[0].x == food.x && snake[0].y == food.y) {
         score++;
+        scoreOnDoc.innerText = score
         appleDrop();
+        appleDrow();
     }
     else {
         snake.pop();
@@ -80,14 +89,19 @@ function snakeMover() {
 }
 function timeManager() {
     setTimeout(() => {
+        clearBoard();
+        appleDrow();
         snakeDrawer();
         snakeMover();
         timeManager();
-    }, 1000)
+    }, 100)
 }
-function snakeCleaner() {
-    snake.pop();
+
+function clearBoard() {
+    ctx.fillStyle = snakeBorder
+    ctx.fillRect(0, 0, 500, 500)
 }
+
 function startGame() {
     snakeDrawer();
     appleDrop();
