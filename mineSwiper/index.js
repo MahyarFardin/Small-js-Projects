33e11b09ec27cb73fa35345425000ca1219e227a
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-15 23:14:54 +0430

    done with table and bombs and numbers

--- mineSwiper/index.js
+++ mineSwiper/index.js
@@ -10,6 +10,7 @@ const start = document.getElementsByClassName("restart-button")[0]
 const board = document.getElementById("board")
 const numberOfBombs = 9
 let bombs = [];
+let boardTable = [];
 let started = true
 
 start.addEventListener("click", () => gameControler())
@@ -19,34 +20,65 @@ function gameControler() {
 
     boardDrawer();
     bombLocator();
+    numbersSetter();
+    bombsSetter();
+}
+
+function numbersSetter() {
+    for (let i = 0; i < 8; i++) {
+        const row = []
+        for (let j = 0; j < 8; j++) {
+            row.push(0)
+        }
+        boardTable.push(row)
+    }
+
+    bombs.forEach(element => {
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                if (element.x + i > -1 && element.y + j > -1 && element.x + i < 8 && element.y + j < 8)
+                    boardTable[element.x + i][element.y + j] += 1;
+            }
+        }
+    })
+}
+
+function bombsSetter() {
+    bombs.forEach(element => {
+        boardTable[element.x][element.y] = -1
+    })
 }
 function cellClickHandler(params) {
     console.log(params.target);
 }
 function bombLocator() {
     for (let i = 0; i < numberOfBombs; i++) {
-        const newNumber = Math.floor(Math.random() * 64)
-        let flag=false
+        const x = Math.floor(Math.random() * 8)
+        const y = Math.floor(Math.random() * 8)
+
+        let flag = false
 
         for (let j = 0; j < bombs.length; j++) {
-            if (bombs[j] === newNumber) {
+            if (bombs[j].x === x && bombs[j].y == y) {
                 i--;
-                flag=true
+                flag = true
                 break;
             }
         }
 
-        if(flag==false)
-            bombs.push(newNumber)
+        if (flag == false)
+            bombs.push({ x: x, y: y })
     }
 }
 
-function boardDrawer(params) {
-    for (let index = 0; index < 64; index++) {
-        const element = document.createElement("button")
-        element.className = "cell " + index
-        element.onclick=e=>cellClickHandler(e)
-        board.appendChild(element)
+function boardDrawer() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            const element = document.createElement("button")
+            element.className = `cell ${i} ${j}`
+            element.onclick = e => cellClickHandler(e)
+            board.appendChild(element)
+        }
     }
 }
 
