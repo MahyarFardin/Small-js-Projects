6072aa2497c09990df353ca1ddd8bb16e23511f8
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-25 14:44:42 +0430

    progressing

--- Sort Algorithm Visualizer/index.js
+++ Sort Algorithm Visualizer/index.js
@@ -1,75 +1,41 @@
-const selector = document.getElementById("selector")
-const numbers = document.getElementById("numbers")
-const randomNumbers = [];
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-selector.addEventListener("change", e => algorithmSelector(e))
+const position = []
+canvas.addEventListener("mousemove", (event) => handleMove(event));
 
-randomNumberGenerator()
-function randomNumberGenerator() {
-    for (let i = 0; i < 25; i++) {
-        randomNumbers.push(Math.floor(Math.random() * 100))
+function handleMove(event) {
+    if (position.length > 100) {
+        position.shift()
     }
-    numberDisplay()
+
+    const temp = [event.clientX, event.clientY]
+    position.push(temp)
+    console.log(position);
+    handleDraw()
+    handlePops()
+    console.log(position);
 }
 
-function numberDisplay() {
-    randomNumbers.map(number => {
-        const element = document.createElement("li")
-        element.id = number
-        element.innerText = number
-        element.style.height = `${number * 5}px`
-        numbers.appendChild(element)
+function handlePops(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            position.shift()
+        },500)
+        resolve(0)
     })
 }
 
-function algorithmSelector(event) {
-    console.log(event.target.value);
-    switch (event.target.value) {
-        case "bubble":
-            bubble();
-        case "quick":
-            quick();
-        case "merge":
-            merge();
-        case "insertion":
-            insertion();
+function handleDraw() {
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = .5;
+
+    // draw a red line
+    ctx.beginPath();
+    ctx.moveTo(position[0][0], position[0][1])
+    for (let i = 1; i < position.length; i++) {
+        ctx.lineTo(position[i][0], position[i][1])
     }
+    ctx.stroke();
 }
 
-function graphicSwap(a, b) {
-    setTimeout(() => {
-        var temp = document.createElement("div");
-        a.parentNode.insertBefore(temp, a);
-s
-        // move obj1 to right before obj2
-        b.parentNode.insertBefore(a, b);
-
-        // move obj2 to right before where obj1 used to be
-        temp.parentNode.insertBefore(b, a);
-
-        // remove temporary marker node
-        temp.parentNode.removeChild(temp);
-    }, 1000)
-}
-
-function bubble() {
-    for (let i = 0; i < 25; i++) {
-        for (let j = i; j < 25; j++) {
-            if (randomNumbers[i] > randomNumbers[j]) {
-                let temp = randomNumbers[i]
-                randomNumbers[i] = randomNumbers[j]
-                randomNumbers[j] = temp
-                graphicSwap(randomNumbers[j], randomNumbers[i])
-            }
-        }
-    }
-}
-function quick() {
-
-}
-function merge() {
-
-}
-function insertion() {
-
-}
